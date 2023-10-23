50b62550a4c022dc0eb1fe79f71fd737a3eacb39
HIBA-BEG <beghiba@gmail.com>
2023-10-23 14:23:26 +0100

    brief-3-desktop

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var firstname = document.getElementById('firstname');
+var lastname = document.getElementById('lastname');
+var cin = document.getElementById('cin');
+var Phone = document.getElementById('Phone');
+
+firstname.addEventListener('input', function(e){
+    console.log(e.target.value)
+})
